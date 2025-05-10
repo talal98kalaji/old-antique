d0535129b089dc1df9e0b582fb82a651e693672f
unknown <talal.kalaji.alep@gmail.com>
2025-05-10 20:42:17 +0300

    github pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import AppContent from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter basename="/old-antique">
+    <Router basename="/old-antique">
       <AppContent />
-    </BrowserRouter>
-  </React.StrictMode>
+    </Router>
 );
